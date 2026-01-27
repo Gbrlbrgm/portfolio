3f53f72ea29a3786f595b05c9f0d84d89965dc39
Gabriel Bergamo <gabriel_bergamo@outlook.com>
2026-01-26 23:52:51 -0300

    feat: add initAOS to home

--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -1,12 +1,28 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import Hero from "./Hero";
 import Services from "./Services";
 import Resume from "./Resume";
 import Projects from "./Projects";
 import Skills from "./Skills";
 import Contact from "./Contact";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Home = () => {
+  useEffect(() => {
+    const initAOS = async () => {
+      await import("aos");
+      AOS.init({
+        duration: 300,
+        easing: "ease",
+        once: true,
+        anchorPlacement: "top-bottom",
+      });
+    };
+
+    initAOS();
+  }, []);
   return (
     <div className="overflow-hidden">
       <Hero />
