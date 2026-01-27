e1624d23b5be225afa2d70c37dcc923d5ca5233d
Gabriel Bergamo <gabriel_bergamo@outlook.com>
2026-01-26 23:58:52 -0300

    feat: add aos to services section

--- components/Home/Services/index.tsx
+++ components/Home/Services/index.tsx
@@ -9,8 +9,13 @@ const Services = () => {
         Lorem Ipsum
       </h1>
       <div className="w-[90%] sm:w-[70%] mx-auto grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-10 mt-20 items-center">
-        {ServicesData.map((service) => (
-          <div key={service.id}>
+        {ServicesData.map((service, i) => (
+          <div
+            data-aos="fade-right"
+            data-aos-anchor-placement="top-center"
+            data-aos-delay={i * 100}
+            key={service.id}
+          >
             <ServiceCard
               icon={service.icon}
               name={service.name}
