a10d79d09b442effb61bbfedb6171e95e678a750
Gabriel Bergamo <gabriel_bergamo@outlook.com>
2026-01-28 19:18:19 -0300

    fix: adjust projects section AOS

--- components/Home/Projects/index.tsx
+++ components/Home/Projects/index.tsx
@@ -22,7 +22,10 @@ const Projects = () => {
   }, []);
   return (
     <div id="projects" className="pt-16 pb-16">
-      <h1 className="text-center text-2xl md:text-4xl xl:text-5xl font-bold text-white">
+      <h1
+        data-aos="fade-in"
+        className="text-center text-2xl md:text-4xl xl:text-5xl font-bold text-white"
+      >
         A small selection of recent <br /> <span>projects</span>
       </h1>
       <div className="w-[70%] mx-auto grid grid-cols-1 md:grid-cols-2 gap-10 mt-16">
