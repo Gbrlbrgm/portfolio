6eb0b710905079af95594cdf3bfd2f5697ac73f9
Gabriel Bergamo <gabriel_bergamo@outlook.com>
2026-01-28 18:48:09 -0300

    fix: service section AOS adjusted

--- components/Home/Services/index.tsx
+++ components/Home/Services/index.tsx
@@ -4,23 +4,16 @@ import { ServicesData } from "@/constant/constant";
 
 const Services = () => {
   return (
-    <div
-      id="services"
-      data-aos="fade-in"
-      data-aos-anchor-placement="top-center"
-      className="pt-32 pb-16"
-    >
-      <h1 className="text-center text-2xl md:text-4xl xl:text-5xl font-bold text-white">
+    <div id="services" className="pt-32 pb-16">
+      <h1
+        data-aos="fade-in"
+        className="text-center text-2xl md:text-4xl xl:text-5xl font-bold text-white"
+      >
         My Services
       </h1>
       <div className="w-[90%] sm:w-[70%] mx-auto grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-10 mt-16 items-center">
         {ServicesData.map((service, i) => (
-          <div
-            data-aos="fade-right"
-            data-aos-anchor-placement="top-center"
-            data-aos-delay={i * 100}
-            key={service.id}
-          >
+          <div data-aos="fade-right" data-aos-delay={i * 100} key={service.id}>
             <ServiceCard
               icon={service.icon}
               name={service.name}
