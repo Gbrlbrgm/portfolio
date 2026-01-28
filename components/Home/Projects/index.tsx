c602b8da83e6e3571965bd973f0c31da93a440b0
Gabriel Bergamo <gabriel_bergamo@outlook.com>
2026-01-28 19:56:20 -0300

    style: add hover to project div

--- components/Home/Projects/index.tsx
+++ components/Home/Projects/index.tsx
@@ -1,6 +1,7 @@
 import { ProjectsNames } from "@/constant/constant";
 import Image from "next/image";
 import React from "react";
+import { CiLink } from "react-icons/ci";
 
 const Projects = () => {
   return (
@@ -13,7 +14,12 @@ const Projects = () => {
       </h1>
       <div className="w-[70%] mx-auto grid grid-cols-1 md:grid-cols-2 gap-10 mt-16">
         {ProjectsNames.map((project) => (
-          <div data-aos="fade-up" key={project.id}>
+          <a
+            href={project.url}
+            target="_blank"
+            data-aos="fade-up"
+            key={project.id}
+          >
             <Image
               src={project.thumb}
               alt="img"
@@ -21,16 +27,14 @@ const Projects = () => {
               height={600}
               className="rounded-lg"
             />
-            <a
-              href={project.url}
-              className="mt-4 text-xl sm:text-2xl font-semibold text-white block"
-            >
+            <p className="mt-4 text-xl sm:text-2xl font-semibold text-white block hover:text-cyan-300 transition-all duration-200">
               {project.name}
-            </a>
+              <CiLink className="inline-block w-6 h-6 ml-1" />
+            </p>
             <h1 className="pt-2 font-medium text-white/80">
               {project.description}
             </h1>
-          </div>
+          </a>
         ))}
       </div>
     </div>
