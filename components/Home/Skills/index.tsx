d676fdfca36014bff325239206ffb7a3c458af91
Gabriel Bergamo <gabriel_bergamo@outlook.com>
2026-01-28 19:43:09 -0300

    fix: adjust AOS on skills section

--- components/Home/Skills/index.tsx
+++ components/Home/Skills/index.tsx
@@ -59,17 +59,19 @@ const skills = [
 const Skills = () => {
   return (
     <div id="skills" className="text-white pt-16 pb-16">
-      <h1 className="text-center text-2xl md:text-4xl xl:text-5xl font-bold text-white">
+      <h1
+        data-aos="fade-in"
+        data-aos-offset="0"
+        className="text-center text-2xl md:text-4xl xl:text-5xl font-bold text-white"
+      >
         My <span className="text-cyan-500">Skills</span>
       </h1>
       <div className="flex flex-wrap justify-center gap-6 mt-16">
-        {skills.map((skill, i) => (
+        {skills.map((skill) => (
           <Tilt key={skill.id} scale={1.2} transitionSpeed={400}>
             <div
               data-aos="flip-right"
-              data-aos-anchor-placement="top-center"
-              data-aos-offset="-300"
-              data-aos-delay={i * 50}
+              data-aos-offset="0"
               className="bg-[#14134145] text-center w-40 h-48 rounded-3xl flex flex-col items-center justify-center shadow-lg transition hover:scale-105"
             >
               <div className="text-5xl mb-4 text-gray-300">{skill.icon}</div>
